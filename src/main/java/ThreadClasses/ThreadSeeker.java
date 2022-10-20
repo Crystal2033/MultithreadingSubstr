d7361e51d45ae8298ebe54cb6fb819020d0d92d7
Pavel <kulikov-pavlik@bk.ru>
2022-10-20 21:15:50 +0300

    reworked thread class a bit. Need to add while loop in it.

--- src/main/java/ThreadClasses/ThreadSeeker.java
+++ src/main/java/ThreadClasses/ThreadSeeker.java
@@ -8,15 +8,22 @@ public class ThreadSeeker implements Runnable {
     private final FileCommunicator fileCommunicator;
     private final int id;
     private int alreadyCheckedBlocks = 0;
+    private final Thread thread;
 
     public ThreadSeeker(int valueOfPrevAndNextLines, FileCommunicator fileCommunicator) {
         this.valueOfPrevAndNextLines = valueOfPrevAndNextLines;
         this.fileCommunicator = fileCommunicator;
         id = threadID++;
+        thread = new Thread("Hobbit" + Integer.toString(id));
     }
 
     @Override
     public void run() {
-        //TODO!
+        System.out.println("Heeeeey, my name is: " + thread.getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
